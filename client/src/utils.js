2cadde4680e0cce03863f13ad03512a332769e17
Marcin Ptok <marcin111100@gmail.com>
2024-01-07 23:40:18 +0100

    Fixed bug with UTC time

--- client/src/utils.js
+++ client/src/utils.js
@@ -7,7 +7,7 @@ export const formatDateTime = (date, time) => {
   const hour = time.$H;
   const minute = time.$m;
 
-  return `${date.$y}-${formatDatePart(month)}-${formatDatePart(day)}T${formatDatePart(hour)}:${formatDatePart(minute)}:00Z`;
+  return `${date.$y}-${formatDatePart(month)}-${formatDatePart(day)}T${formatDatePart(hour)}:${formatDatePart(minute)}:00`;
 };
 
 const formatDatePart = (part) => (part >= 10 ? part : '0' + part);
