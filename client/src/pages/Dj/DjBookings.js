0a22e78e56f358cf765294da838ec1bc32124972
kacpero1407 <kacper.slabik2000@gmail.com>
2024-01-07 23:32:37 +0100

    added removing bookings if expired

--- client/src/pages/Dj/DjBookings.js
+++ client/src/pages/Dj/DjBookings.js
@@ -9,144 +9,171 @@ import moment from 'moment';
 import { getRemainingTime } from '../../utils.js';
 
 function DjBookings() {
-  const [bookings, setBookings] = useState([]);
-  const dispatch = useDispatch();
+	const [bookings, setBookings] = useState([]);
+	const dispatch = useDispatch();
 
-  const handleConfirmExpire = async (id) => {
-    console.log('TODO: Koniec:', id);
-  };
+	const handleConfirmExpire = async (id) => {
+		try {
+			console.log('TODO: Koniec:', id);
 
-  const getBookingsData = async () => {
-    try {
-      dispatch(showLoading());
-      const response = await axios.get('/api/dj/get-bookings-by-dj-id', {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
-      dispatch(hideLoading());
-      if (response.data.success) {
-        toast.success(response.data.message);
-        setBookings(response.data.data);
-      }
-    } catch (error) {
-      toast.error('Błąd pobrania rezerwacji');
-      dispatch(hideLoading());
-    }
-  };
+			const response = await axios.post(
+				'/api/dj/remove-expired-bookings',
+				{},
+				{
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem('token')}`,
+					},
+				}
+			);
 
-  const changeBookingStatus = async (record, status) => {
-    try {
-      dispatch(showLoading());
-      const response = await axios.post(
-        '/api/dj/change-booking-status',
-        { bookingId: record._id, status: status },
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        }
-      );
-      dispatch(hideLoading());
-      if (response.data.success) {
-        toast.success(response.data.message);
-        getBookingsData();
-      }
-    } catch (error) {
-      toast.error('Błąd zmiany statusu konta DJa');
-      dispatch(hideLoading());
-    }
-  };
-  useEffect(() => {
-    getBookingsData();
-  }, []);
+			if (response.data.success) {
+				toast.success(response.data.message);
+				getBookingsData();
+			}
+		} catch (error) {
+			toast.error('Błąd usuwania przedawnionych rezerwacji');
+		}
+	};
 
-  const columns = [
-    {
-      title: 'ID',
-      dataIndex: '_id',
-    },
-    {
-      title: 'Klient',
-      dataIndex: 'name',
-      render: (text, record) => <span>{record.userInfo?.name}</span>,
-    },
-    {
-      title: 'Nr. Telefonu',
-      dataIndex: 'phoneNumber',
-      render: (text, record) => <span>{record.djInfo?.phoneNumber}</span>,
-    },
-    {
-      title: 'Data i czas imprezy',
-      dataIndex: 'createdAt',
-      render: (text, record) => (
-        <span>
-          Rozpoczęcie: {moment(record?.startDate).format('DD-MM-YYYY HH:mm')}
-          <br></br>
-          Zakończenie: {moment(record?.endDate).format('DD-MM-YYYY HH:mm')}
-        </span>
-      ),
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      render: (text, record) => {
-        let tagType = '';
+	const getBookingsData = async () => {
+		try {
+			dispatch(showLoading());
+			const response = await axios.get('/api/dj/get-bookings-by-dj-id', {
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
+			});
+			dispatch(hideLoading());
+			if (response.data.success) {
+				toast.success(response.data.message);
+				setBookings(response.data.data);
+			}
+		} catch (error) {
+			toast.error('Błąd pobrania rezerwacji');
+			dispatch(hideLoading());
+		}
+	};
 
-        switch (text) {
-          case 'Potwierdzona':
-            tagType = 'success';
-            break;
-          case 'Oczekuje':
-            tagType = 'warning';
-            break;
-          case 'Odrzucona':
-            tagType = 'error';
-            break;
-          default:
-            tagType = 'default';
-            break;
-        }
+	const changeBookingStatus = async (record, status) => {
+		try {
+			dispatch(showLoading());
+			const response = await axios.post(
+				'/api/dj/change-booking-status',
+				{ bookingId: record._id, status: status },
+				{
+					headers: {
+						Authorization: `Bearer ${localStorage.getItem('token')}`,
+					},
+				}
+			);
+			dispatch(hideLoading());
+			if (response.data.success) {
+				toast.success(response.data.message);
+				getBookingsData();
+			}
+		} catch (error) {
+			toast.error('Błąd zmiany statusu konta DJa');
+			dispatch(hideLoading());
+		}
+	};
+	useEffect(() => {
+		getBookingsData();
+	}, []);
 
-        return <Tag color={tagType}>{text}</Tag>;
-      },
-    },
-    {
-      title: 'Data dodania',
-      render: (text, record) => <span>{moment(record.createdAt).format('DD-MM-YYYY HH:mm')}</span>,
-    },
-    {
-      title: 'Akcje',
-      dataIndex: 'actions',
-      render: (text, record) => {
-        const [timeText, isEnded] = getRemainingTime(record.createdAt, 2);
-        if (isEnded) handleConfirmExpire(record._id);
-        return (
-          <div className="d-flex" style={{ gap: '10px', alignItems: 'center' }}>
-            {record.status === 'Oczekuje' && (
-              <div className="d-flex" style={{ gap: '10px' }}>
-                <Button type="primary" onClick={() => changeBookingStatus(record, 'Potwierdzona')}>
-                  Potwierdź
-                </Button>
+	const columns = [
+		{
+			title: 'ID',
+			dataIndex: '_id',
+		},
+		{
+			title: 'Klient',
+			dataIndex: 'name',
+			render: (text, record) => <span>{record.userInfo?.name}</span>,
+		},
+		{
+			title: 'Nr. Telefonu',
+			dataIndex: 'phoneNumber',
+			render: (text, record) => <span>{record.djInfo?.phoneNumber}</span>,
+		},
+		{
+			title: 'Data i czas imprezy',
+			dataIndex: 'createdAt',
+			render: (text, record) => (
+				<span>
+					Rozpoczęcie: {moment(record?.startDate).format('DD-MM-YYYY HH:mm')}
+					<br></br>
+					Zakończenie: {moment(record?.endDate).format('DD-MM-YYYY HH:mm')}
+				</span>
+			),
+		},
+		{
+			title: 'Status',
+			dataIndex: 'status',
+			render: (text, record) => {
+				let tagType = '';
 
-                <Button danger onClick={() => changeBookingStatus(record, 'Odrzucona')}>
-                  Odrzuć
-                </Button>
-              </div>
-            )}
-            <span>{timeText}</span>
-          </div>
-        );
-      },
-    },
-  ];
-  return (
-    <Layout>
-      <h1 className="page-title">Rezerwacje</h1>
-      <hr />
-      <Table columns={columns} dataSource={bookings} rowKey="_id" />
-    </Layout>
-  );
+				switch (text) {
+					case 'Potwierdzona':
+						tagType = 'success';
+						break;
+					case 'Oczekuje':
+						tagType = 'warning';
+						break;
+					case 'Odrzucona':
+						tagType = 'error';
+						break;
+					default:
+						tagType = 'default';
+						break;
+				}
+
+				return <Tag color={tagType}>{text}</Tag>;
+			},
+		},
+		{
+			title: 'Data dodania',
+			render: (text, record) => (
+				<span>{moment(record.createdAt).format('DD-MM-YYYY HH:mm')}</span>
+			),
+		},
+		{
+			title: 'Akcje',
+			dataIndex: 'actions',
+			render: (text, record) => {
+				const [timeText, isEnded] = getRemainingTime(record.createdAt, 2);
+				if (isEnded) handleConfirmExpire(record._id);
+				return (
+					<div className='d-flex' style={{ gap: '10px', alignItems: 'center' }}>
+						{record.status === 'Oczekuje' && (
+							<div className='d-flex' style={{ gap: '10px' }}>
+								<Button
+									type='primary'
+									onClick={() => changeBookingStatus(record, 'Potwierdzona')}
+								>
+									Potwierdź
+								</Button>
+
+								<Button
+									danger
+									onClick={() => changeBookingStatus(record, 'Odrzucona')}
+								>
+									Odrzuć
+								</Button>
+							</div>
+						)}
+						<span>{timeText}</span>
+					</div>
+				);
+			},
+		},
+	];
+	return (
+		<Layout>
+			<h1 className='page-title'>Rezerwacje</h1>
+			<hr />
+			<Table columns={columns} dataSource={bookings} rowKey='_id' />
+		</Layout>
+	);
 }
 
 export default DjBookings;
